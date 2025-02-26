1b6ef3f9d0f479f3a7c1f249ec71cbf7ce91585d
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-26 15:22:31 +0530

    new folder functionality

--- folderExplorer/src/App.jsx
+++ folderExplorer/src/App.jsx
@@ -2,37 +2,59 @@
 // Expand and collaps folder
 // Add / remove file/folder
 
-import { useState } from 'react'
-import json from './data.json'
-import './App.css'
-
+import { useState } from "react";
+import json from "./data.json";
+import "./App.css";
 
 // render list of obj
-const List = ({list}) => {
-  const [isExpanded,setIsExpanded] = useState(false)
+const List = ({ list , addNodeToList}) => {
+  const [isExpanded, setIsExpanded] = useState({});
   return (
-    <div className='folders'>
-    {list.map((item)=> (
-      <div key={Math.random()}>
-        {item?.isFolder && <span onClick={()=>setIsExpanded(prev=>!prev)}>{isExpanded ? "-" : "+"} </span>}
-        <span>{item.name}</span>
-      {isExpanded && item?.children && <List list={item.children}/>}
-      </div>
+    <div className="folders">
+      {list.map((item) => (
+        <div key={item.id}>
+          {item.isFolder && (
+            <span onClick={() => setIsExpanded((prev) => ({...prev,[item.name]:!prev[item.name]}))}>
+              {isExpanded?.[item.name] ? "-" : "+"}
+            </span>
+          )}
+          <span>{item.name}</span>
+          {item.isFolder && <span onClick={()=>addNodeToList(item.id)}>
+            <img src="https://cdn-icons-png.flaticon.com/128/4732/4732392.png" alt="folder-logo" className="icon" />
+          </span>}
+          {isExpanded?.[item.name] && item?.children && (
+            <List list={item.children} />
+          )}
+        </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
 function App() {
-  const [data, setData] = useState(json)
+  const [data, setData] = useState(json);
 
+  const addNodeToList = (parentId) => {
+      const updatTree = (list) => {
+         return list.map(node=>{
+           if(node.id === parentId){
+            return {
+              ...node,
+              children:{...node.children,
+                
+              }
+            }
+           }
+         })
+      }
+      setData((prev)=>updatTree(prev))
+  }
   return (
-   <div className='container'>
-    <h1>File/Folder Explorer</h1>
-    <List list={data}/>
-    
-   </div>
-  )
+    <div className="container">
+      <h1>File/Folder Explorer</h1>
+      <List list={data} />
+    </div>
+  );
 }
 
-export default App
+export default App;
